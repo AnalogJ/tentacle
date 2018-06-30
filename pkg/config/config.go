41b142aa043ce6928f559b6307d290bc16a39197
Jason Kulatunga <jason@thesparktree.com>
2018-06-29 18:17:18 -0700

    adding beginning of config file.

---
+++ pkg/config/config.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"bytes"
+	"gopkg.in/yaml.v2"
+	"fmt"
+)
+
+// When initializing this class the following methods must be called:
+// Config.New
+// Config.Init
+// This is done automatically when created via the Factory.
+type configuration struct {
+	*viper.Viper
+}
+
+
+//Viper uses the following precedence order. Each item takes precedence over the item below it:
+// explicit call to Set
+// flag
+// env
+// config
+// key/value store
+// default
+
+func (c *configuration) Init() error {
+	c.Viper = viper.New()
+	//set defaults
+	c.SetDefault("options.config_dir", "~/.ssh/drawbridge")
+
+	//if you want to load a non-standard location system config file (~/drawbridge.yml), use ReadConfig
+	c.SetConfigType("yaml")
+	//c.SetConfigName("drawbridge")
+	//c.AddConfigPath("$HOME/")
+
+	//CLI options will be added via the `Set()` function
+
+	return c.ValidateConfig()
+}
+func (c *configuration) ReadConfig(configFilePath string) error {
+	configFilePath, err := utils.ExpandPath(configFilePath)
+	if err != nil {
+		return err
+	}
+
+	if !utils.FileExists(configFilePath) {
+		log.Printf("No configuration file found at %v. Skipping", configFilePath)
+		return errors.ConfigFileMissingError("The configuration file could not be found.")
+	}
+
+	//validate config file contents
+	err = c.ValidateConfigFile(configFilePath)
+	if err != nil {
+		log.Printf("Config file at `%v` is invalid: %s", configFilePath, err)
+		return err
+	}
+
+	log.Printf("Loading configuration file: %s", configFilePath)
+
+	config_data, err := os.Open(configFilePath)
+	if err != nil {
+		log.Printf("Error reading configuration file: %s", err)
+		return err
+	}
+
+	err = c.MergeConfig(config_data)
+	if err != nil {
+		return err
+	}
+
+	return c.ValidateConfig()
+}
---
+++ pkg/config/interface.go
@@ -0,0 +1,31 @@
+package config
+
+// Create mock using:
+// mockgen -source=pkg/config/interface.go -destination=pkg/config/mock/mock_config.go
+type Interface interface {
+	Init() error
+	ReadConfig(configFilePath string) error
+	Set(key string, value interface{})
+	SetDefault(key string, value interface{})
+	AllSettings() map[string]interface{}
+	IsSet(key string) bool
+	Get(key string) interface{}
+	//GetBool(key string) bool
+	//GetInt(key string) int
+	GetString(key string) string
+	GetStringSlice(key string) []string
+	UnmarshalKey(key string, rawVal interface{}) error
+
+	//GetProvidedAnswerList() ([]map[string]interface{}, error)
+	//InternalQuestionKeys() []string
+	//GetQuestion(questionKey string) (Question, error)
+	//GetQuestions() (map[string]Question, error)
+	////GetQuestionsSchema() (map[string]interface{}, error)
+	////GetQuestionSchema(question Question) (map[string]interface{}, error)
+	//
+	//GetPacTemplate() (template.PacTemplate, error)
+	//GetConfigTemplates() (map[string]template.ConfigTemplate, error)
+	//GetActiveConfigTemplate() (template.ConfigTemplate, error)
+	//GetCustomTemplates() (map[string]template.FileTemplate, error)
+	//GetActiveCustomTemplates() ([]template.FileTemplate, error)
+}
